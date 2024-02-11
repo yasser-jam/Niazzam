b28e154f205fb6d3d14c5b929d76d71872e4b33b
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2024-02-11 23:39:33 +0200

    goals card component

---
+++ components/goal/Card.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card shadow-md mx-auto bg-white rounded-xl relative w-[90%]">
+        <div class="card-body mx-auto">
+            <div class="radial-progress text-2xl" :style="`--value:${value};`" role="progressbar">{{ value }}%</div>
+            <div class="font-semibold text-2xl mt-4">Reading</div>
+
+            <div class="mt-8 mx-auto">
+                <base-btn color="primary">Details</base-btn>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { type, value } = defineProps({
+    type: {
+        type: String,
+        default: 'primary'
+    },
+    value: {
+        type: Number,
+    }
+})
+</script>
---
+++ pages/goals.vue
@@ -0,0 +1,23 @@
+<template>
+  
+    <div class="container">
+        
+      <base-title class="mb-12 mt-8">Goals</base-title>
+      
+      <div class="grid grid-cols-3 gap-x-8">
+        <goal-card class="text-primary" :value="50"></goal-card>
+        <goal-card class="text-primary" :value="50"></goal-card>
+        <goal-card class="text-primary" :value="50"></goal-card>
+      </div>
+  
+      <div class="grid grid-cols-3 gap-x-8 mt-8">
+      </div>
+  
+      <div class="grid mt-8">
+          <!-- <payment-table></payment-table> -->
+      </div>
+    </div>
+  
+  
+  </template>
+  
