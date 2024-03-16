016293e92d902755dc8b192122b4bb1f4db13ebd
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2024-03-17 00:26:54 +0200

    todo section

---
+++ components/todo/Section.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="grid grid-cols-3 gap-8">
+    <da-card class="bg-white rounded-lg p-4">
+      <da-card-title class="mb-2">
+        <div class="flex items-center">
+          <Icon name="heroicons:clock" class="me-2" />
+          <div>Pending</div>
+        </div>
+      </da-card-title>
+
+      <da-card-body class="p-0 h-[200px] overflow-auto">
+        <draggable :list="list" group="todo" item-key="name">
+          <template #item="{ element }">
+            <div
+              class="border-2 bg-gray-100 shadow-sm p-2 my-2 rounded-lg font-medium text-md cursor-pointer"
+            >
+              {{ element.name }}
+            </div>
+          </template>
+        </draggable>
+      </da-card-body>
+    </da-card>
+
+    <da-card class="bg-white rounded-lg p-4">
+      <da-card-title class="mb-2">
+        <div class="flex items-center">
+          <Icon name="heroicons:adjustments-vertical" class="me-2" />
+          <div>Doing</div>
+        </div>
+      </da-card-title>
+
+      <da-card-body class="p-0 h-[200px] overflow-auto">
+        <draggable :list="list2" group="todo" item-key="name">
+          <template #item="{ element }">
+            <div
+              class="border-2 bg-gray-100 shadow-sm p-2 my-2 rounded-lg font-medium text-md cursor-pointer"
+            >
+              {{ element.name }}
+            </div>
+          </template>
+        </draggable>
+      </da-card-body>
+    </da-card>
+
+    <da-card class="bg-white rounded-lg p-4">
+      <da-card-title class="mb-2">
+        <div class="flex items-center">
+          <Icon name="heroicons:check" class="me-2" />
+          <div>Done</div>
+        </div>
+      </da-card-title>
+
+      <da-card-body class="p-0 h-[200px] overflow-auto">
+        <draggable :list="list3" group="todo" item-key="name">
+          <template #item="{ element }">
+            <div
+              class="border-2 bg-gray-100 shadow-sm p-2 my-2 rounded-lg font-medium text-md cursor-pointer"
+            >
+              {{ element.name }}
+            </div>
+          </template>
+        </draggable>
+      </da-card-body>
+    </da-card>
+  </div>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+let id = 1;
+export default {
+  name: "two-list-headerslots",
+  display: "Two list header slot",
+  order: 14,
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      list: [
+        { name: "John 1", id: 0 },
+        { name: "Joao 2", id: 1 },
+        { name: "Jean 3", id: 2 },
+      ],
+      list2: [
+        { name: "Jonny 4", id: 3 },
+        { name: "Guisepe 5", id: 4 },
+      ],
+      list3: [
+        { name: "Jonny 4", id: 3 },
+        { name: "Guisepe 5", id: 4 },
+      ],
+    };
+  },
+  methods: {
+    add: function () {
+      this.list.push({ name: "Juan " + id, id: id++ });
+    },
+    replace: function () {
+      this.list = [{ name: "Edgard", id: id++ }];
+    },
+    add2: function () {
+      this.list2.push({ name: "Juan " + id, id: id++ });
+    },
+    replace2: function () {
+      this.list2 = [{ name: "Edgard", id: id++ }];
+    },
+  },
+};
+</script>
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,29 @@
 <template>
-    <!-- <div>
+  <!-- <div>
         <base-chart></base-chart>
     </div> -->
 
-    <base-subtitle class="mt-8">Tasks</base-subtitle>
-    <div class="grid grid-cols-1">
-        <!-- <payment-table :payments="payments"></payment-table> -->
-    </div>
+  <base-subtitle class="mt-8 mb-4">Tasks</base-subtitle>
+  <div class="grid grid-cols-1">
+    <todo-section></todo-section>
+    <!-- <payment-table :payments="payments"></payment-table> -->
+  </div>
 
-    <base-subtitle class="flex items-center mt-8 mb-4">
-        <div>Outcomes</div>
-        <div class="ms-2 text-gray-400 text-base font-normal">(last week)</div>
-</base-subtitle>
-    <div class="grid grid-cols-1">
-        <payment-table :payments="payments"></payment-table>
-    </div>
-
+  <base-subtitle class="flex items-center mt-8 mb-4">
+    <div>Outcomes</div>
+    <div class="ms-2 text-gray-400 text-base font-normal">(last week)</div>
+  </base-subtitle>
+  <div class="grid grid-cols-1">
+    <payment-table :payments="payments"></payment-table>
+  </div>
 </template>
 
 <script setup lang="ts">
-const test = ref('')
+const test = ref("");
 
-const paymentStore = usePaymentStore()
+const paymentStore = usePaymentStore();
 
-const { payments } = storeToRefs(paymentStore)
+const { payments } = storeToRefs(paymentStore);
 
-const { pending } = useLazyAsyncData(() => paymentStore.list())
-
-
+const { pending } = useLazyAsyncData(() => paymentStore.list());
 </script>
